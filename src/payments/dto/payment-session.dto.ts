1ae676bb196b374614c704fc8cc71b3cf613a28e
AbrilSabatini <abrilsabatini2004@gmail.com>
2024-11-04 15:51:45 -0300

    Endpoint payment session

---
+++ src/payments/dto/payment-session.dto.ts
@@ -0,0 +1,38 @@
+import { Type } from 'class-transformer';
+import {
+  ArrayMinSize,
+  IsArray,
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
+
+export class PaymentSessionDto {
+  @IsString()
+  @IsNotEmpty()
+  currency: string;
+
+  @IsArray()
+  @ArrayMinSize(1)
+  @ValidateNested({ each: true })
+  @Type(() => PaymentSessionItemDto)
+  items: PaymentSessionItemDto[];
+}
+
+export class PaymentSessionItemDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsNumber()
+  @IsPositive()
+  @IsNotEmpty()
+  price: number;
+
+  @IsNumber()
+  @IsPositive()
+  @IsNotEmpty()
+  quantity: number;
+}
